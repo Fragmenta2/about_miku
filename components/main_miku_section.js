d1ed24b990a80c991de1c092c486ce750e170cb7
Luis Lopez (Fragmenta2) <212485916+Fragmenta2@users.noreply.github.com>
2025-09-11 01:20:29 -0600

    New redesign of the page (new look)

--- components/main_miku_section.js
+++ components/main_miku_section.js
@@ -3,14 +3,22 @@ import React from "react";
 export default function Main_miku_section() {
     return (
         <>
-            <p>Hatsune Miku</p>
+            <p className="bebas_neue character_title">HATSUNE_MIKU</p>
             <div className="cellphone_bg">
                 <div className="cellphone_notch"></div>
                 <div className="cellphone_instagram_frame">
-                    <img className="cellphone_instragram_pfp" src="images/miku.jpg"/>
-                    <hr className="cellphone_instragram_frame_bottom_section"/>
+                    <div className="cellphone_instragram_frame_top">
+                        <img className="cellphone_instragram_pfp" src="images/miku.jpg" />
+                    </div>
+                    <div className="cellphone_instragram_frame_bottom">
+                        <img src="images/like.png" />
+                        <img src="images/comment.png" />
+                        <img className="" src="images/saved_post.png" />
+                    </div>
+
                 </div>
-                <p className="orbitron cellphone_text">01</p>
+                
+                <p className="orbitron cellphone_text"><u>01</u></p>
 
             </div>
         </>
