43c3fd3829ae797e79dcc9c5131cc910975ffb6b
Luis Lopez (Fragmenta2) <212485916+Fragmenta2@users.noreply.github.com>
2025-09-12 12:50:42 -0600

    New design and added a menu bar

--- components/main_miku_section.js
+++ components/main_miku_section.js
@@ -1,49 +1,58 @@
 'use client'
-import { useEffect,React,useState } from "react";
+import { useEffect, React, useState } from "react";
 
 export default function Main_miku_section() {
 
-    const [is_ready,set_is_ready] = useState(true);
+    const [is_ready, set_is_ready] = useState(true);
 
 
-    function re_adjust(){
-        let cellphone_bg = document.getElementById('cellphone_bg');
-        let cellphone_miku_sitting = document.getElementById('cellphone_miku_sitting')!=null ? document.getElementById('cellphone_miku_sitting'):0;
-        !(cellphone_miku_sitting == 0) ? cellphone_miku_sitting.style.marginBottom = (cellphone_bg.offsetHeight + 40) +"px":setTimeout(()=>{re_adjust()},10);
+    function re_adjust() {
+        if (document.getElementById('cellphone_miku_sitting') == null) {
+            setTimeout(() => { re_adjust() }, 10)
+        }
+        else {
+            let cellphone_bg = document.getElementById('cellphone_bg');
+            let cellphone_miku_sitting = document.getElementById('cellphone_miku_sitting');
+            cellphone_miku_sitting.style.marginBottom = (cellphone_bg.offsetHeight + 40) + "px";
+        }
     }
-     useEffect(() => {
-      set_is_ready(false);
-    }, []); 
-    
-    if (!is_ready){
-         re_adjust();
-    return (
-        <>
-            <p className="bebas_neue character_title">HATSUNE'MIKU</p>
-            <div className="navigation_menu">
-                <p>Home</p>
-                <p>About</p>
-                <p>Contact</p>
-            </div>
-            <div id="cellphone_bg" className="cellphone_bg">
-                <div className="cellphone_notch"></div>
-                <img id="cellphone_miku_sitting" className="cellphone_miku_sitting" src="images/miku_sit_transparent.png"/>
-                <div className="cellphone_instagram_frame">
-                    <div className="cellphone_instragram_frame_top">
-                    </div>
-                    <div className="cellphone_instragram_frame_bottom">
-                    </div>
 
+
+    useEffect(() => {
+        setTimeout(() => {
+            set_is_ready(false);
+        }, 300);
+    }, []);
+
+    if (!is_ready) {
+        re_adjust();
+        return (
+            <>
+                <p id="character_title" className="bebas_neue character_title">HATSUNE'MIKU</p>
+                <div className="navigation_menu">
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Contact</p>
                 </div>
-                
-                <p className="orbitron cellphone_text"><u>01</u></p>
-                <Usarwindow fun={re_adjust}/>
-            </div>
-        </>
-    );
+                <div id="cellphone_bg" className="cellphone_bg">
+                    <div className="cellphone_notch"></div>
+                    <img id="cellphone_miku_sitting" className="cellphone_miku_sitting" src="images/miku_sit_transparent.png" />
+             
+
+                    <p className="orbitron cellphone_text"><u>01</u></p>
+                    <Usarwindow fun={re_adjust} />
+                </div>
+            </>
+        );
     }
-    else{
-        return(<> hola</>);
+    else {
+        return (
+            <div className="loading_container">
+
+                <span className="loading"></span>
+            </div>
+
+        );
     }
 }
 
@@ -54,7 +63,7 @@ const Usarwindow = (props) => {
         return () => {
             window.removeEventListener("resize", props.fun);
         };
-    }, []); 
+    }, []);
 
     return (
         <>
