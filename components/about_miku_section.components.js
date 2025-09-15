46fa2587b9d53d7e4027d1d2ce60db3dbea77b71
Luis Lopez (Fragmenta2) <212485916+Fragmenta2@users.noreply.github.com>
2025-09-14 19:57:38 -0600

    Making a back up for the incoming changes

--- components/about_miku_section.components.js
+++ components/about_miku_section.components.js
@@ -26,12 +26,13 @@ export default function About_miku_section() {
             </div>
             <h1 id='about_miku_section_title' className="bebas_neue about_miku_section_title">WHO IS HATSUNE MIKU?</h1>
             <div id='main_miku_info_container' className='main_miku_info_container'>
-                <Miku_info url='images/first_miku_slide.jpg' bg_color='rgba(0,0,0,.9)' title='She IS vocaloid' description='Miku is a voice synthesizer software, a virtual singer whose voice can be used by any creator to produce original music and she is the most widely used.' color='white'/>
-                <Miku_info url='images/hatsune_hijole.jpg' bg_color='rgba(0,0,0,.9)' title='She IS a meme' description='Her image is a cornerstone of internet culture, starring in countless viral parodies, trends, and the iconic "Leekspin" animation.' color='white' />
-                <Miku_info url='images/third_miku_slide.jpg' bg_color='rgba(0,0,0,.9)' title='She IS an inspiration' description='She serves as a creative muse for a global community, inspiring countless musicians, animators, and artists to create and share original works.' color='white'/>
-                <Miku_info url='images/fourth_miku_slide.jpg' bg_color='rgba(0,0,0,.9)' title='She IS an IDOL' description='A true virtual pop star, Miku headlines sold-out concerts around the globe, performing live on stage as a stunning 3D projection.' color='white'/>
+                <Miku_info url='images/first_miku_slide.jpg' bg_color='rgba(0,0,0,.6)' title='She IS vocaloid' description='Miku is a voice synthesizer software, a virtual singer whose voice can be used by any creator to produce original music and she is the most widely used.' color='white'/>
+                <Miku_info url='images/hatsune_hijole.jpg' bg_color='rgba(0,0,0,.6)' title='She IS a meme' description='Her image is a cornerstone of internet culture, starring in countless viral parodies, trends, and the iconic "Leekspin" animation.' color='white' />
+                <Miku_info url='images/third_miku_slide.jpg' bg_color='rgba(0,0,0,.6)' title='She IS an inspiration' description='She serves as a creative muse for a global community, inspiring countless musicians, animators, and artists to create and share original works.' color='white'/>
+                <Miku_info url='images/fourth_miku_slide.jpg' bg_color='rgba(0,0,0,.6)' title='She IS an IDOL' description='A true virtual pop star, Miku headlines sold-out concerts around the globe, performing live on stage as a stunning 3D projection.' color='white'/>
             </div>
-            <img className='about_miku_section_miku_image' src='https://i.pinimg.com/originals/e5/fb/35/e5fb354990e452a0f2cf8e62d28847b0.png'></img>
+            <img className='about_miku_section_maleta' src='images/maleta.png'></img>
+            <img className='about_miku_section_miku_image' src='images/llavero_miku_transparente.png'></img>
             <Usarwindow fun={re_adjust_about_miku} />
         </div>
     );
