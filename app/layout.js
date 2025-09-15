8ab3eeeed800d984f64811d3de5b77352d4fc15c
Luis Lopez (Fragmenta2) <212485916+Fragmenta2@users.noreply.github.com>
2025-09-15 00:17:31 -0600

    FINAL VERSION (I added an icon an title)

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <title>Miku Page</title>
       <body>{children}</body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -3,10 +3,14 @@ import React from "react";
 
 import Main_miku_section from "@/components/main_miku_section.components.js";
 
+export const metadata = {
+  title: 'Miku Page',
+};
+
 export default function Home() {
   return (
     <div className="main_container">
-      <Main_miku_section/>
+      <Main_miku_section />
     </div>
   );
 }
